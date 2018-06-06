1fc59bf83c3b78c21451e84ad95edcacd4da840b
lucns <luckyfire1996@gmail.com>
2018-06-06 14:54:13 +0700

    LucNS: thêm login register function.

--- app/Http/Controllers/Mobile/UserController.php
+++ app/Http/Controllers/Mobile/UserController.php
@@ -9,18 +9,82 @@
 namespace App\Http\Controllers\Mobile;
 
 
+use App\Model\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
+use Mockery\Exception;
 
 class UserController extends Controller
 {
 
     /**
      * @param Request $request
+     * @return json
      */
-    public function login(Request $request){
+    public function login(Request $request)
+    {
+        try {
+            $error = new \stdClass();
+            $phone = $request->input('phone');
+            $password = $request->input('password');
+            Log::info("LOGIN " . $phone);
+            Log::info("LOGINI2" . $password);
+            $user = User::where('phone', $phone)
+                ->where('password', $password)
+                ->first();
+            if ($user != null) {
+                return response()->json($user, 200);
+            } else {
+                $error->error = "Số điện thoại hoặc mật khẩu không chính xác";
+                $error->exception = "No exception";
+                return response()->json($error, 400);
+            }
+        } catch (Exception $ex) {
+            $error->error = "Đã xảy ra lỗi";
+            $error->exception = $ex;
+            return response()->json($ex, 400);
+        }
+    }
 
-        
+    public function loginGET()
+    {
+        return response()->json(['hello' => 'cha co gi ca haha'], 200);
+    }
 
+    public function register(Request $request)
+    {
+        try {
+            $user = User::where('phone', $request->input('phone'))
+                ->first();
+            if ($user != null) {
+                $error = new \stdClass();
+                $error->error = "Số điện thoại đã tồn tại";
+                $error->exception = "No Exception";
+                return response()->json($error, 200);
+            } else {
+                $user = new User();
+                $user->phone = $request->input('phone');
+                $user->password = $request->input('password');
+                ////HASH
+                $user->isActive = 1;
+                $user->isDelete = 0;
+                $user->save();
+                return response()->json($user, 200);
+            }
+        } catch (\Exception $ex) {
+            $error = new \stdClass();
+            $error->error = "Không thể đăng kí thông tin người dùng";
+            $error->exception = $ex;
+            return response()->json($error, 200);
+        }
+    }
+
+    public
+    function bookAppointment(Request $request)
+    {
+        ///lclclclcl
+
+        return response()->json([$request->all()], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post("user/login", "Mobile\UserController@login");
+Route::get("user/login", "Mobile\UserController@loginGET");
+Route::post("user/register", "Mobile\UserController@register");
+Route::post("user/bookAppointment", "Mobile\UserController@bookAppointment");
