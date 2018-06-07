b49101e7c44e5bfc11d409b9142387a29110822e
lucns <luckyfire1996@gmail.com>
2018-06-07 23:40:01 +0700

    LucNS:update news controller

---
+++ app/Http/Controllers/Mobile/TreatmentCategoryController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luc
+ * Date: 07-Jun-18
+ * Time: 19:47
+ */
+
+namespace App\Http\Controllers\Mobile;
+
+
+use App\Http\Controllers\Controller;
+
+class TreatmentCategoryController extends Controller
+{
+    public function getAll()
+    {
+            //treatment
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,10 @@ Route::post("user/login", "Mobile\UserController@login");
 Route::get("user/login", "Mobile\UserController@loginGET");
 Route::post("user/register", "Mobile\UserController@register");
 Route::post("user/bookAppointment", "Mobile\UserController@bookAppointment");
+
+Route::get("city/all", "Mobile\AddressController@getAllCitites");
+Route::get("city/{id}/districts/", "Mobile\AddressController@getDistrictsByCity");
+Route::get("news/all", "Mobile\NewsController@getAllNews");
+Route::get("news/loadmore", "Mobile\NewsController@loadMore");
+//treatment category
+Route::get("treatmentcategory/all", "Mobile\TreatmentCategoryController@getAll");
