a8c2cad81b16b5b6366d46c7c15fcbc24d8a1deb
lucns <luckyfire1996@gmail.com>
2018-06-13 23:53:58 +0700

    LucNS: createAppointment bussiness function da test

--- app/Http/Controllers/BusinessFunction/AppointmentBussinessFunction.php
+++ app/Http/Controllers/BusinessFunction/AppointmentBussinessFunction.php
@@ -17,67 +17,77 @@ use Mockery\Exception;
 
 trait AppointmentBussinessFunction
 {
-    public function createAppointment($phone, $note, $datebooking)
+
+    public function createAppointment($phone, $note, $bookingDate)
     {
         try {
             $appointmentDB = new Appointment();
             $listAppointment = $appointmentDB->where(
-                'date_booking', ">=",
-                Carbon::today()->toDateString());
-
-            usort($listAppointment->toArray(), "sortByTimeStamp");
-            $topTenElement = $listAppointment->take(10);
-
-            $min = PHP_INT_MAX;
-            $minAppointment = new \stdClass();
-            foreach ($topTenElement as $item) {
-                $currentTimeStamp = $this->getAppointmentTimeStamp($item);
-                if ($currentTimeStamp < $min) {
-                    $min = $currentTimeStamp;
-                    $minAppointment = $item;
+                'start_time', ">=",
+                Carbon::today()->toDateString())->get();
+            $predictAppointmentDate = new \DateTime();
+            $estimatedTime = new \DateTime("00:30");
+            $numericalOrder = $listAppointment->count() + 1;
+            if ($listAppointment->count() < 3) {
+                $bookingDateObj = new \DateTime($bookingDate);
+                $predictAppointmentDate = $this->addTimeToDate($bookingDateObj, "07:00:00");
+            } else {
+                $appointmentArray = $listAppointment->toArray();
+                usort($appointmentArray, array($this, "sortByTimeStamp"));
+                $topElement = array_slice($appointmentArray, 0, 3);
+                $maxdate = new \DateTime("2035-12-12");
+                $min = $maxdate->getTimestamp() + $maxdate->getTimestamp();
+                $minAppointment = array();
+                foreach ($topElement as $item) {
+                    $appointmentTimeStamp = $this->getAppointmentTimeStamp($item);
+                    if ($appointmentTimeStamp < $min) {
+                        $min = $appointmentTimeStamp;
+                        $minAppointment = $item;
+                    }
                 }
+                // $predictAppointmentDate= the finish datetime of the previous patient;
+                $predictAppointmentDate = new \DateTime($minAppointment['start_time']);
+//            $minAppointmentTime = new \DateTime($minAppointment['estimated_time']);
+//            $intervalDate = $minAppointmentDate->diff(new \DateTime("00-00-00"))
+                $intervalTime = new \DateInterval('P0000-00-00T' . $minAppointment['estimated_time']);
+
+                $predictAppointmentDate->add($intervalTime);
             }
-
-            $minAppointmentDate = new \DateTime($minAppointment->date_booking);
-            $minAppointmentTime = new \DateTime($minAppointment->estimated_time);
-
-            $intervalTime = $minAppointmentTime->diff(new \DateTime("00:00:00"));
-
-            $minAppointmentDate->add($intervalTime);
-
             $appointment = new Appointment();
             $appointment->phone = $phone;
             $appointment->note = $note;
-            $appointment->date_booking = $minAppointmentDate;
-            $appointment->estimated_time = new \DateTime("00:30");
-            $appointment->numerical_order = $listAppointment->count();
-//        ->format("Y-m-d H:i:s");
-
+            $appointment->estimated_time = $estimatedTime;
+            $appointment->start_time = $predictAppointmentDate;
+            $appointment->date_booking = $bookingDate;
+            $appointment->numerical_order = $numericalOrder;
             $appointment->save();
             return $appointment;
-        }catch (Exception $exception){
+
+        } catch (Exception $exception) {
+//            $exception->getTrace();
             return null;
         }
     }
 
+    private function addTimeToDate($date, $time)
+    {
+        $intervalTime = new \DateInterval('P0000-00-00T' . $time);
+        $date->add($intervalTime);
+        return $date;
+    }
+
     private function sortByTimeStamp($appointment1, $appointment2)
     {
-//        $dateTimeAppointment1 = new \DateTime($appointment1->date_booking);
-//        $timeAppointment1 = new \DateTime($appointment1->estimated_time);
-////        $dateTimeAppointment2 = new \DateTime($appointment2->date_booking);
-//        $timeAppointment2 = new \DateTime($appointment2->estimated_time);
-//        $timestampAP1 = $dateTimeAppointment1->getTimestamp() + $timeAppointment1->getTimestamp();
-//        $timestampAP2 = $dateTimeAppointment2->getTimestamp() + $timeAppointment2->getTimestamp();
 
         $timestampAP1 = $this->getAppointmentTimeStamp($appointment1);
         $timestampAP2 = $this->getAppointmentTimeStamp($appointment2);
-        return $timestampAP1 > $timestampAP2;
+        return $timestampAP1 < $timestampAP2;
     }
 
     private function getAppointmentTimeStamp($appointment)
     {
-        $dateTimeAppointment = new \DateTime($appointment->date_booking);
-        $timeAppointment = new \DateTime($appointment->estimated_time);
+        $dateTimeAppointment = new \DateTime($appointment['start_time']);
+        $timeAppointment = new \DateTime($appointment['estimated_time']);
         $timestampAP = $dateTimeAppointment->getTimestamp() + $timeAppointment->getTimestamp();
         return $timestampAP;
     }
