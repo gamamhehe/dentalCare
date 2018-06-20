12627a4a6126894bd9ea18ad5a42490f95f8eef3
lucns <luckyfire1996@gmail.com>
2018-06-20 20:50:22 +0700

    LucNS: commit test function

--- app/Http/Controllers/Mobile/MobileController.php
+++ app/Http/Controllers/Mobile/MobileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Mobile;
 
+use App\Model\Patient;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\Exception\RequestExceptionInterface;
@@ -29,19 +30,38 @@ class MobileController extends Controller
         try {
             if ($request->hasFile('image')) {
                 $phone = $request->input('phone');
-                $image = $request->file('image');
-                $path = public_path() . '/photos/avatar';
-                $filename = 'user_avatar_'.$phone. '.'.$image->getClientOriginalExtension();
+                $patient = Patient::where('phone', $phone)->first();
+                if ($patient != null) {
+
+                    $image = $request->file('image');
+                    $path = public_path('\photos\avatar');
+                    $filename = 'user_avatar_' . $phone . '.' . $image->getClientOriginalExtension();
 //                dd($filename);
-                $fullpath = $path .$filename;
-                $image->move($path, $filename);
+                    $fullPath = implode('/',array_filter(explode('/', $path .$filename)));
+                    $image->move($path, $filename);
 //                $post->image = $path;
-                return response($fullpath, 200);
+                    $patient->avatar = $filename;
+                    $patient->save();
+                    $response = new \stdClass();
+                    $response->message = "Thay đổi ảnh đại diện thành công";
+                    $response->status ="OK";
+                    $response->data = $fullPath;
+                    return response()->json($response, 200);
+                } else {
+                    $error = new \stdClass();
+                    $error->error = "Không thể tìm thấy số điện thoại " . $phone;
+                    $error->exception = "Nothing";
+                    return response()->json($error, 400);
+                }
             } else {
 
+                $error = new \stdClass();
+                $error->error = "Lỗi khi nhận hình ảnh " ;
+                $error->exception = "Nothing";
+                return response()->json($error, 400);
             }
-        }catch(\Exception $ex){
-            return response()->json('php sida'.$ex->getMessage());
+        } catch (\Exception $ex) {
+            return response()->json('php sida' . $ex->getMessage());
         }
     }
 
