663214aec0b393c1f7fad93a196557047d013c69
lucns <luckyfire1996@gmail.com>
2018-06-28 16:06:52 +0700

    LucNS: update send notification firebase

--- app/Http/Controllers/Mobile/FirebaseController.php
+++ app/Http/Controllers/Mobile/FirebaseController.php
@@ -9,6 +9,7 @@
 namespace App\Http\Controllers\Mobile;
 
 
+use App\Http\Controllers\BusinessFunction\UserBusinessFunction;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
@@ -16,6 +17,8 @@ use Illuminate\Http\Request;
 
 class FirebaseController extends Controller
 {
+    use UserBusinessFunction;
+
     public function sendNotification(Request $request)
     {
         try {
@@ -23,35 +26,41 @@ class FirebaseController extends Controller
             $body = $request->input("body");
             $title = $request->input("title");
             $message = $request->input("message");
-            $token = $request->input("to");
+            $phone = $request->input("phone");
             $requestObj = new \stdClass();
-
+            $user = $this->getUserByPhone($phone);
+            if ($user == null) {
+                $error = new \stdClass();
+                $error->error = "Không thể tìm thấy số điện thoại";
+                $error->exception = null;
+                return response()->json($error, 400);
+            }
+            $token = $user->notif_token;
             $data = new \stdClass();
             $data->type = $type;
             $data->body = $body;
             $data->title = $title;
             $data->message = $message;
 
-
             $requestObj->data = $data;
-
             $requestObj->to = $token;
-
+            $firebaseServerToken = env('API_FIREBASE_SERVER_TOKEN', false);
             $client = new Client();
             $request = $client->request('POST', 'https://fcm.googleapis.com/fcm/send',
                 [
                     'body' => json_encode($requestObj),
-                    'headers'=>[
-                    'Content-Type' => 'application/json',
-                    'authorization' => 'key=AAAAUj5G2Bc:APA91bF8TkhDriuoevyt_I0G3G-qNniLSDdDHbULjcvsas4sHCuTKueiODRnuvVuYk6YkCHKLt3fr-Sw7UhZMzRSfmWMWzt2NZXzljYZxch39fg0v3NsBzQM5_QKUEy4bOdnnjigzaBX'
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'authorization' => $firebaseServerToken
+                    ]
                 ]
-                ]
-            ); 
+            );
             $response = $request->getBody()->getContents();
-            return response()->json($response);
+            $responseObj = json_decode($response);
+            return response()->json($responseObj, 200);
         } catch (GuzzleException $ex) {
             $error = new \stdClass();
-            $error->error = $ex->getTrace();
+            $error->error = "Lỗi máy chủ";
             $error->exception = $ex->getMessage() . " File: " . $ex->getFile() . " Line: " . $ex->getLine();
             return response()->json($error, 500);
         }
