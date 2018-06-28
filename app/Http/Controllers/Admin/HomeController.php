cc62d97d07a52492e5a19bbb395a54b3c71f291c
Huynh Tai <huynhtaicr@gmail.com>
2018-06-28 20:08:05 +0700

    fix-menu

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -66,7 +66,7 @@ class HomeController extends Controller
         return view("WebUser.User.MyProfile");
     }
     public function aboutUs(Request $request){
-        return view("WebUser.User.aboutUs");
+        return view("WebUser.User.AboutUs");
     }
 
 
