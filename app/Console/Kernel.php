730622d50e263721886030fa72364b2dd1a870e2
lucns <luckyfire1996@gmail.com>
2018-06-30 23:00:38 +0700

    LucNS: reminder scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RemindAppointment;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Psy\Command\Command;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\RemindAppointment::class,
     ];
 
     /**
@@ -26,6 +28,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('remind:appointment')
+            ->everyTenMinutes();
     }
 
     /**
