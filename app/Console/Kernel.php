9537785a8f53219fa8bcb2daaf526f8c781d8a7f
lucns <luckyfire1996@gmail.com>
2018-06-30 23:25:51 +0700

    LucNS: everyminute kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('remind:appointment')
-            ->everyTenMinutes();
+            ->everyMinute();
     }
 
     /**
