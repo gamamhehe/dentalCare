b7af3d613f33526c03977a83f718717024d0fc14
Huynh Tai <huynhtaicr@gmail.com>
2018-07-02 22:34:44 +0700

    merge

--- app/Http/Controllers/BusinessFunction/TreatmentBusinessFunction.php
+++ app/Http/Controllers/BusinessFunction/TreatmentBusinessFunction.php
@@ -17,7 +17,7 @@ use App\Model\TreatmentHistory;
 use App\Model\TreatmentImage;
 use App\Model\User;
 use Carbon\Carbon;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 trait TreatmentBusinessFunction
 {
@@ -40,59 +40,6 @@ trait TreatmentBusinessFunction
 
         }
     }
-    public function getTreatmentHistory($id)
-    {
-        $listResult = [];
-        $patient = Patient::where('id', $id)->first();
-        $treatmentHistoryList = $patient->hasTreatmentHistory()->get();
-
-
-//            foreach ($treatmentHistoryList as $treatmentHistory) {
-//                $treatmentHistoryDetailList = $treatmentHistory->hasTreatmentDetail()->get();
-//
-//                foreach ($treatmentHistoryDetailList as $treatmentHistoryDetail){
-//
-//                    $treatmentHistoryDetail->dentist_id = $treatmentHistoryDetail->belongsToStaff()->first();
-//                }
-//                foreach ($treatmentHistoryDetailList as $treatmentHistoryDetail){
-//                    $treatmentHistoryDetail->step = $treatmentHistoryDetail->hasTreatmentDetailStep()->first()->belongsToStep()->first();
-//                }
-
-
-        foreach ($treatmentHistoryList as $treatmentHistory) {
-            $treatmentHistoryDetailList = $treatmentHistory->hasTreatmentDetail()->get();
-            foreach ($treatmentHistoryDetailList as $treatmentHistoryDetail) {
-                $treatmentHistoryDetail->dentist = $treatmentHistoryDetail->belongsToStaff()->first();
-                $treatmentHistoryDetail->treatment_images = $treatmentHistoryDetail->hasTreatmentImage()->get();
-                $treatmentMedicines = $treatmentHistoryDetail->hasMedicinesQuantity()->get();
-                foreach ($treatmentMedicines as $treatmentMedicine) {
-                    $treatmentMedicine->medicine = $treatmentMedicine->belongsToMedicine()->first();
-                }
-                $treatmentDetailSteps = $treatmentHistoryDetail->hasTreatmentDetailStep()->get();
-                foreach ($treatmentDetailSteps as $treatmentDetailStep) {
-                    $treatmentDetailStep->step = $treatmentDetailStep->belongsToStep()->first();
-                }
-                //add property to object
-                $treatmentHistoryDetail->prescriptions = $treatmentMedicines;
-                $treatmentHistoryDetail->treatment_detail_steps = $treatmentDetailSteps;
-            }
-            $treatmentHistory->details = $treatmentHistoryDetailList;
-            $treatmentHistory->treatment = $treatmentHistory->belongsToTreatment()->first();
-            $treatmentHistory->patient = $patient;
-            $treatmentHistory->tooth = $treatmentHistory->belongsToTooth()->first();
-            $treatmentHistory->payment = $treatmentHistory->belongsToPayment()->first();
-
-//
-//                foreach ($treatmentHistoryDetailList as $treatmentHistoryDetail){
-//                    $treatmentHistoryDetail->step = $treatmentHistoryDetail->hasTreatmentDetailStep()->get();
-//
-//                }
-
-//        }
-//        dd($treatmentHistoryList);
-        }
-        return $treatmentHistoryList;
-    }
     public function createTreatment($input){
         DB::beginTransaction();
         try{
@@ -114,15 +61,6 @@ trait TreatmentBusinessFunction
         $Treatment = Treatment::find($id);
         return $Treatment;
     }
-    public function getTreatmentHistoryByPatientId($id)
-    {
-        $patient = Patient::where('id', $id)->first();
-        if ($patient != null) {
-            $treatmentHistories = $patient->hasTreatmentHistory()->get();
-            return $treatmentHistories;
-        }
-        return null;
-    }
     public function editTreatment($input,$id){
         DB::beginTransaction();
         try{
@@ -141,120 +79,6 @@ trait TreatmentBusinessFunction
         }
     }
 
-    public function getTreatmentHistories($phone)
-    {
-        $treatmentHistories = Patient::where('phone', $phone)->first()->hasTreatmentHistory()->get();
-        return $treatmentHistories;
-    }
-
-    public function saveTreatmentHistory($treatmentHistory)
-    {
-        DB::beginTransaction();
-        try {
-            $treatmentHistory->save();
-            DB::commit();
-            return true;
-
-        } catch (\Exception $e) {
-            DB::rollback();
-            return false;
-
-        }
-    }
-
-    public function createTreatmentProcess($idTreatment, $idPatient, $toothNumber, $price, $description)
-    {
-        DB::beginTransaction();
-        try {
-            $patient = Patient::find($idPatient);
-            $phone = $patient->belongsToUser()->first()->phone;
-            $payment = $this->checkPaymentIsDone($phone);
-            $percentDiscountOfTreatment = $this->checkDiscount($idTreatment);
-            $total_price = $price - $price * $percentDiscountOfTreatment / 100;
-            if ($payment) {
-                $this->updatePayment($total_price, $payment->id);
-                $idPayment = $payment->id;
-            } else {
-                $idPayment = $this->createPayment($total_price, $phone);
-            }
-            $idTreatmentHistory = TreatmentHistory::create([
-                'treatment_id' => $idTreatment,
-                'patient_id' => $idPatient,
-                'description' => $description,
-                'create_date' => Carbon::now(),
-                'tooth_number' => $toothNumber,
-                'price' => $price,
-                'total_price' => $total_price,
-                'payment_id' => $idPayment,
-            ])->id;
-            DB::commit();
-            return $idTreatmentHistory;
-
-        } catch (\Exception $e) {
-            DB::rollback();
-            return false;
-
-        }
-
-    }
-
-    public function createTreatmentDetail($idTreatmentHistory, $note, $dentist_id)
-    {
-        DB::beginTransaction();
-        try {
-            $idTreatmentDetail = TreatmentDetail::create([
-                'treatment_history_id' => $idTreatmentHistory,
-                'dentist_id' => $dentist_id,
-                'note' => $note,
-                'create_date' => Carbon::now()
-            ])->id;
-            DB::commit();
-            return $idTreatmentDetail;
-        } catch (\Exception $e) {
-            DB::rollback();
-            return false;
-
-        }
-    }
-
-    public function createTreatmentDetailStep($listStep, $idTreatmentDetail, $description)
-    {
-        DB::beginTransaction();
-        try {
-            foreach ($listStep as $step) {
-                TreatmentDetailStep::create([
-                    'treatment_detail_id' => $idTreatmentDetail,
-                    'treatment_step_id' => $step->id,
-                    'description' => $description,
-                ]);
-            }
-            DB::commit();
-            return true;
-        } catch (\Exception $e) {
-            DB::rollback();
-            return false;
-
-        }
-    }
-
-    public function createTreatmentImage($imageLink, $idTreatmentDetail)
-    {
-        DB::beginTransaction();
-        try {
-            TreatmentImage::create([
-                'treatment_detail_id' => $idTreatmentDetail,
-                'image_link' => $imageLink,
-                'create_date' => Carbon::now(),
-            ]);
-            DB::commit();
-            return true;
-        } catch (\Exception $e) {
-            DB::rollback();
-            return false;
-
-        }
-    }
-
     public function showTreatmentStepForTreatment($idTreatment)
     {
         $listTreatmentStep = Treatment::find($idTreatment)->hasTreatmentStep()->get();
@@ -263,23 +87,5 @@ trait TreatmentBusinessFunction
             $result[] = $treatmentStep->belongsToStep()->first();
         }
         return $result;
-    }
-
-    public function showTreatmentDetailStepDone($idTreatmentHistory)
-    {
-        $treatmentHistory = TreatmentHistory::find($idTreatmentHistory);
-        $listTreatmentDetail = $treatmentHistory->hasTreatmentDetail()->get();
-        $result = [];
-        foreach ($listTreatmentDetail as $treatmentDetail) {
-            $listTreatmentDetailStep = $treatmentDetail->hasTreatmentDetailStep()->get();
-            foreach ($listTreatmentDetailStep as $treatmentDetailStep) {
-                $result[] = $treatmentDetailStep->treatment_step_id;
-            }
-        }
-        return $result;
-    }
-
-    public function createMedicineForTreatmentDetailBusiness(){
-
     }
 }
