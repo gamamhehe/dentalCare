58a1680afd799cbfb4db92ab777c958599641b76
lucns <luckyfire1996@gmail.com>
2018-07-02 19:53:10 +0700

    LucNS: fix register import

--- app/Http/Controllers/BusinessFunction/PatientBusinessFunction.php
+++ app/Http/Controllers/BusinessFunction/PatientBusinessFunction.php
@@ -11,6 +11,8 @@ namespace App\Http\Controllers\BusinessFunction;
 
 use App\Model\Patient;
 use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 trait PatientBusinessFunction
 {
