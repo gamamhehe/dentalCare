3eee399f82d22b57aef9e69f4f3a915494190100
Huynh Tai <huynhtaicr@gmail.com>
2018-07-03 20:17:23 +0700

    add

---
+++ app/Http/Controllers/BusinessFunction/KeyBusinessFunction.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gamamhehe
+ * Date: 6/17/2018
+ * Time: 3:34 PM
+ */
+
+namespace App\Http\Controllers\BusinessFunction;
+
+
+use App\Model\Absent;
+use App\Model\Role;
+use App\RequestAbsent;
+use Carbon\Carbon;
+
+trait KeyBusinessFunction
+{
+}
