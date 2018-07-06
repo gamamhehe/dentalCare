b98df441b5b02ebb798976068f2dd278a4a50b5a
lucns <luckyfire1996@gmail.com>
2018-07-07 01:34:53 +0700

    LucNS: fix const

--- app/Helpers/AppConst.php
+++ app/Helpers/AppConst.php
@@ -23,10 +23,10 @@ class AppConst
     const MSG_LOGOUT_SUCCESS = "Đăng xuất thành công";
 
 
-    const PERSONAL_CLIENT_ID = 1;
+    const PERSONAL_CLIENT_ID = 'PERSONAL_CLIENT_ID';
     const PERSONAL_CLIENT_SECRET = 'PASSWORD_CLIENT_SECRET';
 
-    const PASSWORD_CLIENT_ID = 2;
+    const PASSWORD_CLIENT_ID = 'PASSWORD_CLIENT_ID';
     const PASSWORD_CLIENT_SECRET = 'PASSWORD_CLIENT_SECRET';
 //0 vua tao
 //1 dang kham
