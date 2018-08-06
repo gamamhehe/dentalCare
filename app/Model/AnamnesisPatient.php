ff1d1c3f5221a58c54e205a3d82e788141913c91
lucns <luckyfire1996@gmail.com>
2018-08-06 17:11:39 +0700

    LucNS: anamnesis

--- app/Model/AnamnesisPatient.php
+++ app/Model/AnamnesisPatient.php
@@ -8,11 +8,47 @@ class AnamnesisPatient extends Model
 {
     //
     protected $table = 'tbl_anamnesis_patients';
+    protected $primaryKey = ['patient_id', 'anamnesis_id'];
+    public $incrementing = false;
     protected $fillable = ['patient_id', 'anamnesis_id', 'description'];
     public function belongsToAnamnesisCatalog(){
         return $this->belongsTo('App\Model\AnamnesisCatalog', 'anamnesis_id', 'id');
     }
     public function belongsToPatient(){
         return $this->belongsTo('App\Model\Patient', 'patient_id', 'id');
+    }
+
+
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
     }
 }
