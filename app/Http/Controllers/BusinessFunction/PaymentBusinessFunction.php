48f8c5a4c84b90f892583e04dfa210b1d6f4f596
lucns <luckyfire1996@gmail.com>
2018-08-07 08:11:13 +0700

    LucNS: fix chart revenue

--- app/Http/Controllers/BusinessFunction/PaymentBusinessFunction.php
+++ app/Http/Controllers/BusinessFunction/PaymentBusinessFunction.php
@@ -170,10 +170,10 @@ trait PaymentBusinessFunction
         $data = DB::select(DB::raw("SELECT sum(total_price) as total_price, staff_id , subquery.name as staff_name FROM( 
 SELECT th.id, th.total_price,td.staff_id, staff.name as name FROM tbl_treatment_histories 
 as th JOIN tbl_treatment_details as td ON th.id = td.treatment_history_id JOIN tbl_staffs as staff ON staff.id = td.staff_id 
-WHERE MONTH(th.created_date) = 7 AND YEAR(th.created_date) = 2018 
+WHERE MONTH(th.created_date) = :month AND YEAR(th.created_date) = :year 
 GROUP BY th.id, td.treatment_history_id, th.total_price,td.staff_id,staff.name
  ORDER BY td.created_date ASC ) AS subquery 
- GROUP BY staff_id"),
+ GROUP BY staff_id,subquery.name "),
             array(
                 'month' => $monthInNumber,
                 'year' => $yearInNumber
