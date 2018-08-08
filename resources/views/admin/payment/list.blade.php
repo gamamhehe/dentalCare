5e2e0d5649b1ebb8b173f6a3d247493e3dd607f1
Huynh Tai <huynhtaicr@gmail.com>
2018-08-08 23:40:51 +0700

    DONE

--- resources/views/admin/payment/list.blade.php
+++ resources/views/admin/payment/list.blade.php
@@ -6,9 +6,9 @@
             <div class="panel panel-default" style="">
                 <div class="panel-heading">
                     <div class="row">
-                        <div class="col-sm-5" style="text-align: left">Danh Sách Chi Trả  <a href="#" class="create-modal btn btn-success btn-sm">
-                                        <i class="glyphicon glyphicon-plus">XXX</i>
-                                    </a></div>
+                        <div class="col-sm-5" style="text-align: left">Danh Sách Chi Trả <a href="#" class="create-modal btn btn-success btn-sm">
+                                <i class="glyphicon glyphicon-plus">XXX</i>
+                            </a></div>
                     </div>
                 </div>
                 <div class="panel-body">
@@ -24,37 +24,37 @@
                         </div>
 
                     </div>
-  <div id="create" class="modal fade" role="dialog">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title"></h4>
-                            </div>
-                            <div class="modal-body">
-                                <form class="form-horizontal" role="form">
-                                    <div class="form-group row add">
-                                        <label class="control-label col-sm-2" for="title">Tên :</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="name" name="name"
-                                                   placeholder="Your name Here" required>
-                                            <p class="error text-center alert alert-danger hidden"></p>
+                    <div id="create" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title"></h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form class="form-horizontal" role="form">
+                                        <div class="form-group row add">
+                                            <label class="control-label col-sm-2" for="title">Số tiền :</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" id="name" name="name"
+                                                       placeholder="Your name Here" required>
+                                                <p class="error text-center alert alert-danger hidden"></p>
+                                            </div>
                                         </div>
-                                    </div>
-                                 
-                                </form>
-                            </div>
-                            <div class="modal-footer">
-                                <button class="btn btn-warning" type="submit" id="add" onclick="save()" >
-                                    <span class="glyphicon glyphicon-plus"></span>Save Post
-                                </button>
-                                <button class="btn btn-warning" type="button" data-dismiss="modal">
-                                    <span class="glyphicon glyphicon-remobe"></span>Close
-                                </button>
+
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-warning" type="submit" id="add" onclick="save()">
+                                        <span class="glyphicon"></span>Thanh toán
+                                    </button>
+                                    <button class="btn btn-warning" type="button" data-dismiss="modal">
+                                        <span class="glyphicon glyphicon-remobe"></span>Đóng
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered Mytable-hover" style="text-align: center">
@@ -115,17 +115,18 @@
     </html> -->
 @endsection
 @section('js')
-<link rel="stylesheet" href="/assets/user/css/mycss.css">
+    <link rel="stylesheet" href="/assets/user/css/mycss.css">
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script>
-        $(document).on('click','.create-modal', function() {
-        $('#create').modal('show');
-        $('.form-horizontal').show();
-        $('.modal-title').text('Add Post');
-    });
+        $(document).on('click', '.create-modal', function () {
+            $('#create').modal('show');
+            $('.form-horizontal').show();
+            $('.modal-title').text('Add Post');
+        });
+
         function search() {
 
             var searchValue = document.getElementById('search').value;
