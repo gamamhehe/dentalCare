9a1129ab4aa8df4c89907c6e14824b082e757cd7
lucns <luckyfire1996@gmail.com>
2018-08-13 23:23:40 +0700

    LucNS: add test deleteUser

--- app/Model/UserHasRole.php
+++ app/Model/UserHasRole.php
@@ -2,17 +2,53 @@
 
 namespace App\Model;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class UserHasRole extends Model
 {
     //
     protected $table = 'tbl_user_has_role';
+    protected $primaryKey = ['phone', 'role_id'];
     protected $fillable = ['phone', 'role_id', 'start_time', 'end_time'];
     public function belongsToUser(){
         return $this->belongsTo('App\Model\User', 'phone', 'phone');
     }
     public function belongsToRole(){
         return $this->belongsTo('App\Model\Role', 'role_id', 'id');
+    }
+
+
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
     }
 }
