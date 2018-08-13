6552343230f143c80f5a07af76093dca72662163
lucns <luckyfire1996@gmail.com>
2018-08-13 16:04:53 +0700

    LucNS: fix tm create symptom

--- app/Http/Controllers/BusinessFunction/TreatmentHistoryBusinessFunction.php
+++ app/Http/Controllers/BusinessFunction/TreatmentHistoryBusinessFunction.php
@@ -93,7 +93,7 @@ trait TreatmentHistoryBusinessFunction
         return $treatmentHistories;
     }
 
-    public function createTreatmentHistory($treatmentHistory, $detailNote, $detailStepIds, $medicines, $symptomIds, $images)
+    public function createTreatmentHistory($treatmentHistory, $detailNote, $detailStepIds, $medicines, $symptoms, $images)
     {
 
         DB::beginTransaction();
