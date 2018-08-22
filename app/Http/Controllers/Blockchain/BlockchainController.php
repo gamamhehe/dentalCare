72dde3efc704f3fc3ee3bc9f6e6d6ab430f7f121
Huynh Tai <huynhtaicr@gmail.com>
2018-08-22 13:54:16 +0700

    testSync

--- app/Http/Controllers/Blockchain/BlockchainController.php
+++ app/Http/Controllers/Blockchain/BlockchainController.php
@@ -282,7 +282,7 @@ class BlockchainController extends Controller
         $publicKey = $this->ReadPublickey();
         // $publicKey = "-----BEGIN PUBLIC KEY-----\r\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCu/Fzjzta9P4X5eg58uJCYM2Dq\r\nkBDixMsJXaywsrJNRwl4W4BB7Zck98q7NXmwa6kNHv8qIrLNgEpMhL5hBt+dVeSH\r\nHoutfhft9DTEaBbu7wrtoR1FmqxgpWhNO6CxKgVE480blf0mwBRI9CAvwqiuedAh\r\nQbSdRm8+v08YjhapVwIDAQAB\r\n-----END PUBLIC KEY-----";
 
-        $dataPaymentDetail = $payment->id . "," . $payment->payment_id . "," . $payment->staff_id . "," . $payment->date_create . "," . $payment->received_money . ',3';
+        $dataPaymentDetail = $payment->id . "," . $payment->payment_id . "," . $payment->staff_id . "," . $payment->created_date . "," . $payment->received_money . ',3';
         $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
         $method = 'aes-256-cbc';
         $encrypted = base64_encode(openssl_encrypt($dataPaymentDetail, $method, '1', OPENSSL_RAW_DATA, $iv));
