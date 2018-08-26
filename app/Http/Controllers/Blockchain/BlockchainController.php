5d7a9629d78047967e3b05a1c06cd0a874178494
Huynh Tai <huynhtaicr@gmail.com>
2018-08-26 19:16:56 +0700

    fix

--- app/Http/Controllers/Blockchain/BlockchainController.php
+++ app/Http/Controllers/Blockchain/BlockchainController.php
@@ -177,11 +177,10 @@ class BlockchainController extends Controller
             $arrayString = explode(',', $element);
             if ($arrayString[sizeof($arrayString) - 1] == 1) {
                 $this->setDataCreatePayment($arrayString);
-                var_dump($arrayString);
             } else if ($arrayString[sizeof($arrayString) - 1] == 2) {
-//                $this->setDataUpdatePayment($arrayString);
+                $this->setDataUpdatePayment($arrayString);
             } else {
-//                $this->setDataPaymentDetail($arrayString);
+                $this->setDataPaymentDetail($arrayString);
             }
         }
     }
